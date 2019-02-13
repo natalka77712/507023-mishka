1c2dcb9d6734d1a4d23e9156b8100c1966925e0f
Евраз <kostousik@mail.ru>
2019-02-13 20:13:55 +0300

    Выполняет задание по препроцессорам и автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,28 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
-gulp.task("style", function () {
-  return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+gulp.task("css", function () {
+return gulp.src("source/less/style.less")
+.pipe(plumber())
+.pipe(less())
+.pipe(postcss([
+autoprefixer()
+]))
+.pipe(gulp.dest("source/css"))
+.pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function () {
-  server.init({
-    server: "./source",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+gulp.task("server", function () {
+server.init({
+server: "source/",
+notify: false,
+open: true,
+cors: true,
+ui: false
 });
+
+gulp.watch("source/less/**/*.less", gulp.series("css"));
+gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("start", gulp.series("css", "server"));
